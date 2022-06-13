7706a5dc7a3d1f01851c11b4bb25b1c2cedb61c5
barredel <bar.redel@campus.technion.ac.il>
2022-06-13 21:20:37 +0300

    Initial commit

---
+++ src/Queue.java
@@ -0,0 +1,11 @@
+public interface Queue<E extends Cloneable> extends Iterable<E>, Cloneable
+{
+    void enqueue(E element);
+    E dequeue();
+    E peek();
+    int size();
+    boolean isEmpty();
+    Queue<E> clone();
+}
+
+
