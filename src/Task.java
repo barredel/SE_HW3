e8da4ba03bb67f8285c40d8290c1b8b822afc1d6
barredel <bar.redel@campus.technion.ac.il>
2022-06-17 01:45:11 +0300

    1ח

--- src/Task.java
+++ src/Task.java
@@ -14,4 +14,50 @@ public class Task implements Cloneable
     {
         return date;
     }
+
+    @Override
+    public boolean equals(Task otherTask)
+    {
+        if((this.description.equals(otherTask.description))&& (this.date.compareTo(otherTask.date)==0))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        String string = description + ", ";
+        if(date.getDate() <10)
+        {
+            string = string + "0" + date.getDate();
+        }
+        else
+        {
+            string = string + "." + date.getDate();
+        }
+        if(date.getMonth() <9)
+        {
+            string = string + ".0" + (date.getMonth()+1);
+        }
+        else
+        {
+            string = string + "." + (date.getMonth()+1);
+        }
+        string = string + "." + (date.getYear() +1900);
+        return string;
+    }
+
+    @Override
+
+    public int hashcode()
+    {
+        return description.hashCode();
+    }
+
+
+
+
+
 }
