7747c33565e3fce6654fe12e86ac920f583656a0
Bar <bar.muller@campus.technion.ac.il>
2022-06-17 03:38:37 +0300

    E

--- src/Task.java
+++ src/Task.java
@@ -1,10 +1,11 @@
+import java.util.Date;
+
 public class Task implements Cloneable
 {
     private String description;
-    private int day;
-    private int month;
-    private int year;
-
+    private Date date;
 
 
 }
+
+
